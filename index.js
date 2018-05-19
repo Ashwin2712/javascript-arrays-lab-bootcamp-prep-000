4eb28da0da0fb67270b35d90f02fca41c9d16d5c
Ashwin George Philip <ashwinphilip27@gmail.com>
2018-05-19 07:08:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-kittens = ['Milo', 'Otis', 'Garfield']
+function kittens() {
+  kittens = ['Milo', 'Otis', 'Garfield']
+}
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens
 }
